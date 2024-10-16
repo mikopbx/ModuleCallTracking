1640ffb4ae180843b012f2e7ae36dee63fd697f5
Nikolay Beketov <nbek@miko.ru>
2024-10-16 11:17:40 +0700

    Refactor checkbox usage
    Replaced redundant checkbox code with a new `addCheckBox` method across multiple forms for consistency and maintainability. Updated relevant dependencies and forms to ensure proper integration, and fixed minor issues in JavaScript files.

--- App/Forms/ModuleCallTrackingForm.php
+++ App/Forms/ModuleCallTrackingForm.php
@@ -1,37 +1,53 @@
 <?php
-/**
- * Copyright © MIKO LLC - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Alexey Portnov, 12 2018
+
+/*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2024 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 namespace Modules\ModuleCallTracking\App\Forms;
+
 use Phalcon\Forms\Element\Check;
 use Phalcon\Forms\Element\Text;
 use Phalcon\Forms\Form;
 
-
 class ModuleCallTrackingForm extends Form
 {
-
     public function initialize($entity = null, $options = null): void
     {
         $this->add(new Text('url'));
-        // is_post
-        $arrCheck = ['value' => null];
-        if ($entity->is_post) {
-            $arrCheck = ['checked' => 'checked', 'value' => null];
-        }
-
-        $this->add(new Check('is_post', $arrCheck));
-
-        // is_1c
-        $arrCheck = ['value' => null];
-        if ($entity->is_1c) {
-            $arrCheck = ['checked' => 'checked', 'value' => null];
-        }
-
-        $this->add(new Check('is_1c', $arrCheck));
+        $this->addCheckBox('is_post', intval($entity->is_post) === 1);
+        $this->addCheckBox('is_1c', intval($entity->is_1c) === 1);
         $this->add(new Text('auth_basic'));
+    }
+
+    /**
+     * Adds a checkbox to the form field with the given name.
+     * Can be deleted if the module depends on MikoPBX later than 2024.3.0
+     *
+     * @param string $fieldName The name of the form field.
+     * @param bool $checked Indicates whether the checkbox is checked by default.
+     * @param string $checkedValue The value assigned to the checkbox when it is checked.
+     * @return void
+     */
+    public function addCheckBox(string $fieldName, bool $checked, string $checkedValue = 'on'): void
+    {
+        $checkAr = ['value' => null];
+        if ($checked) {
+            $checkAr = ['checked' => $checkedValue,'value' => $checkedValue];
+        }
+        $this->add(new Check($fieldName, $checkAr));
     }
 }
